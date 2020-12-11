67bc96f8250265fa3f96c6574aab92bc8bc77ef4
yuxiang_wen <wenyvxiang@iiasaas.com>
2020-12-11 13:29:44 +0800

    add:直连方式调用

---
+++ test-demo/01-c-url-8080/src/main/java/com/wyx/c/url/coderconfig/DepartCodeConfig.java
@@ -0,0 +1,13 @@
+package com.wyx.c.url.coderconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class DepartCodeConfig {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ test-demo/01-p-url-8081/src/main/java/com/wyx/p/url/repository/DepartRepository.java
@@ -0,0 +1,8 @@
+package com.wyx.p.url.repository;
+
+import com.wyx.p.url.bean.Depart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DepartRepository extends JpaRepository<Depart, Integer> {
+}
